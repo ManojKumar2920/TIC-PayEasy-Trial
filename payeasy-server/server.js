53f1cabdc678a76a8f690dac3f0400abdcae90ef
Manoj <manojkumararumainathan@gmail.com>
2024-11-28 11:43:23 +0530

    CORS - Allow origin

--- payeasy-server/server.js
+++ payeasy-server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: 'https://tic-payeasy.netlify.app', 
     credentials: true
   }));
 app.use(express.json());
