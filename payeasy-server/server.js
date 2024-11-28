61b52a85ff6a49df403961e2b99f7dc90bb11505
Manoj <manojkumararumainathan@gmail.com>
2024-11-28 17:13:20 +0530

    CORS Policy

--- payeasy-server/server.js
+++ payeasy-server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://tic-payeasy.vercel.app', 
+    origin:[ 'https://tic-payeasy.vercel.app','http://localhost:5173'], 
     credentials: true
   }));
 app.use(express.json());
