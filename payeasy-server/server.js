1e1e4036f4ed9a7838dcdbe3e12f2f6e7d9a773c
Manoj <manojkumararumainathan@gmail.com>
2024-11-28 11:55:58 +0530

    CORS origin config

--- payeasy-server/server.js
+++ payeasy-server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://tic-payeasy.netlify.app', 
+    origin: 'https://tic-payeasy.vercel.app', 
     credentials: true
   }));
 app.use(express.json());
